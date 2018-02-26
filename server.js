e916d3f04555462bfacaf907c2e9056bb1413b4d
Sherry <lishuxian0738@gmail.com>
2018-02-26 16:43:03 +1100

    2nd try to prod

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ let app = express();
 let cors = require('cors');
 const PORT = process.env.PORT || 3000;
 let express = require('express');
+let bodyParser = require('body-parser');
 
 app.use(cors());
-
+app.use(bodyParser.json());
 
 
 app.get('/details', (req, res) => {
     res.json({info: 'message from express!'});
 });
 
+app.post('/details', (req, res) => {
+    console.log(res.body.info)
+})
 app.listen(PORT, () => console.log('server started on port: ${PORT}'));

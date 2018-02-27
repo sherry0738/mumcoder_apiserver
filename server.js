b9597850392edd12ab8077ceb5bb4568e295e666
Sherry <lishuxian0738@gmail.com>
2018-02-27 13:07:06 +1100

    hard_coding_data for testing

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-
+let express = require('express');
 let app = express();
 let cors = require('cors');
 const PORT = process.env.PORT || 3000;
-let express = require('express');
+
 let bodyParser = require('body-parser');
 
 app.use(cors());
@@ -10,10 +10,33 @@ app.use(bodyParser.json());
 
 
 app.get('/details', (req, res) => {
-    res.json({info: 'message from express!'});
+    res.json(
+        {
+            "questions": [
+                {
+                    "title": "abc",
+                    "description": "abc",
+                    "answers": [
+                        { "usera": "a1" },
+                        { "userb": "a2" },
+                        { "userc": "a3" }
+                    ]
+                },
+                {
+                    "title": "efg",
+                    "description": "abc",
+                    "answers": [
+                        { "user1": "b1" },
+                        { "user2": "b2" },
+                        { "user3": "b2" }
+                    ]
+                }
+            ]
+        }
+    );
 });
 
 app.post('/details', (req, res) => {
     console.log(res.body.info)
 })
-app.listen(PORT, () => console.log('server started on port: ${PORT}'));
+app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
